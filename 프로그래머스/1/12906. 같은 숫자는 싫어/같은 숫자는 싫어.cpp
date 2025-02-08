8d7d873eb807b611a41822119377f0579904e398
KimWandeuk <57186242+hot5667@users.noreply.github.com>
2025-02-08 22:30:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.13 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -3,22 +3,23 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr)  //[1,1,3,3,0,1,1]
+vector<int> solution(vector<int> arr) 
 {
-	vector<int> answer;
-
-	if (arr.empty())
-		return answer;
-
-	answer.push_back(arr[0]);
-
-	for (int i = 1; i < arr.size(); ++i)
-	{
-		if (arr[i] != arr[i - 1])
-		{
-			answer.push_back(arr[i]);
-		}
-	}
-
-	return answer;
+    vector<int> answer;
+    
+    // 빈 배열 예외 처리
+    if(arr.empty())
+        return answer;
+    
+    // 첫 번째 요소 추가
+    answer.push_back(arr[0]);
+    
+    // 이전 값과 다른 때만 추가
+    for(int i = 1; i < arr.size(); ++i)
+    {
+        if(arr[i] != arr[i - 1])
+            answer.push_back(arr[i]);
+    }
+    
+    return answer;
 }
